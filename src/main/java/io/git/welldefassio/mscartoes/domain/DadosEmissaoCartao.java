4c10e797d5d24d7ef17d7a7f983da16cfb60b87d
Wellington Defassio <89281805+WellingtonDefassio@users.noreply.github.com>
2023-01-25 16:02:01 -0300

    recebimento de menssagem rabbitmq

---
+++ src/main/java/io/git/welldefassio/mscartoes/domain/DadosEmissaoCartao.java
@@ -0,0 +1,15 @@
+package io.git.welldefassio.mscartoes.domain;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class DadosEmissaoCartao {
+
+    private Long idCartao;
+    private String cpf;
+    private String endereco;
+    private BigDecimal limiteLiberado;
+
+}
--- src/main/java/io/git/welldefassio/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ src/main/java/io/git/welldefassio/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -1,17 +1,40 @@
 package io.git.welldefassio.mscartoes.infra.mqueue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.git.welldefassio.mscartoes.domain.Cartao;
+import io.git.welldefassio.mscartoes.domain.ClienteCartao;
+import io.git.welldefassio.mscartoes.domain.DadosEmissaoCartao;
+import io.git.welldefassio.mscartoes.infra.repositories.CartaoRespository;
+import io.git.welldefassio.mscartoes.infra.repositories.ClienteCartaoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmissaoCartaoSubscriber {
 
+    private final CartaoRespository cartaoRespository;
+    private final ClienteCartaoRepository clienteCartaoRepository;
+
     @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
-    public void receberSolicitacaoEmissao(@Payload String payload){
-        System.out.println(payload);
+    public void receberSolicitacaoEmissao(@Payload String payload) {
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            DadosEmissaoCartao dadosEmissaoCartao = mapper.readValue(payload, DadosEmissaoCartao.class);
+            Cartao cartao = cartaoRespository.findById(dadosEmissaoCartao.getIdCartao()).orElseThrow();
+
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dadosEmissaoCartao.getCpf());
+            clienteCartao.setLimite(dadosEmissaoCartao.getLimiteLiberado());
+
+            clienteCartaoRepository.save(clienteCartao);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
-
 }
